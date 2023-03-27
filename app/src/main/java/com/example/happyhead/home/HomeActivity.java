2c8d6144f644d4130ff4ba6b3cf8072726b0491f
Prash0265 <kmrprashanth92@gmail.com>
2023-03-27 12:18:50 -0400

    Created wallpaper fragment and layout

--- app/src/main/java/com/example/happyhead/home/HomeActivity.java
+++ app/src/main/java/com/example/happyhead/home/HomeActivity.java
@@ -11,48 +11,48 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import com.example.happyhead.R;
-//import com.pacman.MentAlly.ui.menu.WallpaperFragment;  TO DO
+import com.example.happyhead.menu.WallpaperFragment;
 
 import java.util.Random;
 
 public class HomeActivity extends MainActivity {
-//    private int theme;
-//    Button d;
-//
-//    private TextView quote;
-//    private QuotesModel mQuotes = new QuotesModel();
-//
-//    @Override
-//    protected void onCreate(@Nullable Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        final FrameLayout contentFrameLayout = findViewById(R.id.frag_container);
-//        d = findViewById(R.id.def);
-//
-//        WallpaperFragment w = new WallpaperFragment();
-//        this.theme = w.theme;
-//        getLayoutInflater().inflate(R.layout.wallpaperlayout, contentFrameLayout);
-//
-//        quoteAnimation();
-//        if (this.theme != -1) {
-//            getLayoutInflater().inflate(R.layout.wallpaperlayout, contentFrameLayout);
-//            AppCompatImageView view = findViewById(R.id.imageView);
-//            view.setImageResource(this.theme);
-//        }
-//        else{
-//            getLayoutInflater().inflate(R.layout.defaultwallpaperlayout, contentFrameLayout);
-//
-//        }
-//    }
-//    public void quoteAnimation(){
-//        quote = (TextView) findViewById(R.id.motivational_quote);
-//        setQuote();
-//        Animation aniFade = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
-//        quote.startAnimation(aniFade);
-//    }
-//
-//    public void setQuote(){
-//        Random num = new Random();
-//        int quoteNum = num.nextInt(mQuotes.getLength());
-//        quote.setText(mQuotes.getQuote(quoteNum));
-//    }
+    private int theme;
+    Button d;
+
+    private TextView quote;
+    private QuotesModel mQuotes = new QuotesModel();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final FrameLayout contentFrameLayout = findViewById(R.id.frag_container);
+        d = findViewById(R.id.def);
+
+        WallpaperFragment w = new WallpaperFragment();
+        this.theme = w.theme;
+        getLayoutInflater().inflate(R.layout.wallpaperlayout, contentFrameLayout);
+
+        quoteAnimation();
+        if (this.theme != -1) {
+            getLayoutInflater().inflate(R.layout.wallpaperlayout, contentFrameLayout);
+            AppCompatImageView view = findViewById(R.id.imageView);
+            view.setImageResource(this.theme);
+        }
+        else{
+            getLayoutInflater().inflate(R.layout.defaultwallpaperlayout, contentFrameLayout);
+
+        }
+    }
+    public void quoteAnimation(){
+        quote = (TextView) findViewById(R.id.motivational_quote);
+        setQuote();
+        Animation aniFade = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        quote.startAnimation(aniFade);
+    }
+
+    public void setQuote(){
+        Random num = new Random();
+        int quoteNum = num.nextInt(mQuotes.getLength());
+        quote.setText(mQuotes.getQuote(quoteNum));
+    }
 }
